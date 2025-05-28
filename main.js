69008c4b6afbcd12ddf765c7e1421e702ab844cf
maheshpatil369 <maheshpatilmp937@gmail.com>
2025-05-28 08:45:30 +0530

    just tp

---
+++ main.js
@@ -0,0 +1,31 @@
+// // const, var, let
+
+// const accountId = 144553
+// let accountEmaiil = "mahesh12@google.com"
+// var accountPassword = "12345"
+// let accontCity = "Jaipur"
+
+// // accountId = 2
+
+// accountEmaiil = "mah@gmail.com"
+// accountPassword = "212121212"
+// accontCity = "Bengluru"
+
+// console.log(accountId);
+// console.table([accountId, accountEmaiil, accountPassword, accontCity]);
+
+// /* 
+
+// Prefer not to use var 
+// because of issue in block and functional scope 
+// */
+
+// number => 2 to power 53
+
+// console.log(typeof "***")
+
+
+ let isloggedIn = "hitesh"
+
+ let booleanIsLoogIn = Boolean(isloggedIn)
+ console.log(booleanIsLoogIn);
